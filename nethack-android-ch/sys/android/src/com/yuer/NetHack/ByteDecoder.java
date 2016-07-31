bee30d6168a31e02a085585f14835e62cbb66116
SuperWangCC <ddllkl@hotmail.com>
2016-07-31 12:24:18 +0800

    nethack-mirror

--- nethack-android-ch/sys/android/src/com/yuer/NetHack/ByteDecoder.java
+++
@@ -1,7 +0,0 @@
-package com.yuer.NetHack;
-
-public interface ByteDecoder
-{
-	char decode(int b);
-	String decode(byte[] bytes);
-}
--- nethack-android-ch/sys/android/src/com/yuer/NetHack/DEBUG.java
+++
@@ -1,7 +0,0 @@
-package com.yuer.NetHack;
-
-public class DEBUG
-{
-	public static boolean isOn() { return false; }
-	public static boolean runTrace() { return false; }
-}
